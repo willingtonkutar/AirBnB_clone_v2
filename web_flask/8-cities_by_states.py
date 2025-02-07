a10870fde17ecf656d1f9acd78abe9bbf2bc476a
willingtonkutar <willykutar@gmail.com>
2025-02-07 20:15:46 +0300

    ammends on every file

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
+starts a Flask web applicationstart Flask application"""
 
-"""
 from flask import Flask, render_template
-from models.state import State
+from models import *
 from models import storage
-
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    """
-
-    """
-    storage.close()
-
-
 @app.route('/cities_by_states', strict_slashes=False)
-def states_list():
-    """
-    State list
-    """
-    states = storage.all(State)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
